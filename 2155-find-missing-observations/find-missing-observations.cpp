df094b3222beb3bdb11014ff094370f2e3974463
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-05 12:55:54 +0800

    Time: 166 ms (5.15%) | Memory: 116.4 MB (69.12%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m=rolls.size();
+        int sum=0;
+        for(int i=0;i<m;i++)
+        {
+            sum+=rolls[i];
+        }
+        int require=mean*(n+m);
+        int diff=require-sum;
+        if(diff<n||diff>6*n)
+        {
+            return {};
+        }
+        vector<int> result(n);
+        int avg=diff/n;
+        int remain=diff%n;
+        for(int i=0;i<n;i++)
+        {
+            result[i]=avg;
+            if(remain>0)
+            {
+                result[i]+=min(6-avg,remain);
+                remain-=min(6-avg,remain);
+                cout<<result[i]<<" ";
+            }
+        }
+
+
+        cout<<endl<<remain<<endl;
+
+        return result;
+
+
+    }
+};
