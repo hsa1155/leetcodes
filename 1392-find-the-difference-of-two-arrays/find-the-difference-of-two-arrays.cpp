6b3551c1fe5b1c34a5edb48b5f1848eff346d7ff
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-07 02:36:10 +0800

    Time: 16 ms (99.13%) | Memory: 29.5 MB (98.12%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -7,33 +7,31 @@ public:
         vector<vector<int>> result(2);
         int n1=nums1.size();
         int n2=nums2.size();
-        unordered_set<int> s1,s2;
-
+        bitset<2001> s1,s2,tmp;//-1000=[0] 1000=[2000] x=[x+1000]
         for(int i=0;i<n1;i++)
         {
-            s1.insert(nums1[i]);
+            s1.set(nums1[i]+1000);
         }
-
         for(int i=0;i<n2;i++)
         {
-            s2.insert(nums2[i]);
-            s1.erase(nums2[i]);
+            s2.set(nums2[i]+1000);
         }
-        for(int i=0;i<n1;i++)
-        {
-            s2.erase(nums1[i]);
-        }
+        tmp=s1&s2;
+        s1^=tmp;
+        s2^=tmp;
 
-        for(int x:s1)
+        for(int i=0;i<=2000;i++)
         {
-            result[0].push_back(x);
+            if(s1[i])
+            {
+                result[0].push_back(i-1000);
+            }
+            if(s2[i])
+            {
+                result[1].push_back(i-1000);
+            }
         }
-        for(int x:s2)
-        {
-            result[1].push_back(x);
-        }
-
         return result;
-
+        
     }
 };
