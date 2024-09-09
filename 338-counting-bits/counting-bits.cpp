557aa3f28c28118efbe76d29a5c87b17db7f9826
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-09 23:10:01 +0800

    Time: 3 ms (70.44%) | Memory: 9.3 MB (46.93%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        if(n==0)
+        {
+            return {0};
+        }
+        if(n==1)
+        {
+            return {0,1};
+        }
+        vector<int> ans(n+1);
+        ans[0]=0;
+        ans[1]=1;
+
+        for(int i=2;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                ans[i]=ans[i/2];
+            }
+            else
+            {
+                ans[i]=ans[i/2]+1;
+            }
+        }
+        return ans;
+        
+    }
+};
