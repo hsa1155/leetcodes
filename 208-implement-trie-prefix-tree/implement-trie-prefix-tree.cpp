deb21274806177ab4c0dfd4ed2ca4b3d683df444
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-13 01:08:36 +0800

    Time: 71 ms (20.40%) | Memory: 51.9 MB (17.17%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,104 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Trie {
+private:
+vector<Trie*> children;
+char val='-';
+bool isend=false;
+public:
+    Trie() {
+        children=vector<Trie*>(26,nullptr);
+    }
+    
+    void insert(string word) {
+        //cout<<word<<endl<<endl;;
+        int len=word.length();
+        Trie* current=this;
+        if(len==0)
+        {
+            return;
+        }
+        for(int i=0;i<len;i++)
+        {
+            if(current->children[word[i]-'a']==nullptr)
+            {
+                Trie* child=new Trie();
+                current->children[word[i]-'a']=child;
+                current=child;
+                child->val=word[i];
+            }
+            else
+            {
+                current=current->children[word[i]-'a'];
+            }
+
+        }
+        current->isend=true;
+        
+        
+    }
+    
+    bool search(string word) {
+        Trie* current=this;
+        int len=word.length();
+        bool flag=true;
+
+        for(int i=0;i<len;i++)
+        {
+            if(current->children[word[i]-'a']==nullptr)
+            {
+                flag=false;
+                break;
+            }
+            else
+            {
+                current=current->children[word[i]-'a'];
+            }
+        }
+        if(!current->isend)
+        {
+            flag=false;
+        }
+
+
+
+        return flag;
+        
+    }
+    
+    bool startsWith(string prefix) {
+        cout<<prefix<<endl;
+        Trie* current=this;
+        int len=prefix.length();
+        bool flag=true;
+
+        for(int i=0;i<len;i++)
+        {
+            if(current->children[prefix[i]-'a']==nullptr)
+            {
+                flag=false;
+                break;
+            }
+            else
+            {
+                current=current->children[prefix[i]-'a'];
+            }
+        }
+
+
+
+
+        return flag;
+
+        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
