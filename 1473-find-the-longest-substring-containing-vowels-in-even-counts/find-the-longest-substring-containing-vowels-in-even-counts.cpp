04c9bdccb9651283a1f4db382c578cf4f2a6d709
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-15 16:08:21 +0800

    Time: 90 ms (25.58%) | Memory: 18.9 MB (32.79%) - LeetSync

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        vector<int> bitmask(26,0);
+        bitmask['a'-'a']=1<<0;
+        bitmask['e'-'a']=1<<1;
+        bitmask['i'-'a']=1<<2;
+        bitmask['o'-'a']=1<<3;
+        bitmask['u'-'a']=1<<4;
+        map<int,int> lastSeen;//first=xorvalue second=index
+        lastSeen[0]=-1;
+        int len=s.length();
+        int xorvalue=0;
+        int longest=0;
+
+        for(int i=0;i<len;i++)
+        {
+
+            xorvalue^=bitmask[s[i]-'a'];
+            int current;
+
+            if(xorvalue==0)
+            {
+                current=i+1;
+            }
+            else if(lastSeen.count(xorvalue)==0)
+            {
+                lastSeen[xorvalue]=i;
+                current=0;
+            }
+            else
+            {
+                current=i-lastSeen[xorvalue];
+            }
+            
+            longest=max(longest,current);
+        }
+
+        return longest;
+
+        
+    }
+};
