59ca72086c18ecb369dbb6d9b62023965a0947f4
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-22 21:01:00 +0800

    Time: 9 ms (6.69%) | Memory: 18.1 MB (7.76%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,59 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int index=0;
+        char current=strs[0][0];
+
+        while (1)
+        {
+            bool isprefix=true;
+            if(index>=strs[0].length())
+            {
+                break;
+            }
+            else
+            {
+                current=strs[0][index];
+            }
+            for(string str:strs)
+            {
+                if(index>=str.length())
+                {
+                    isprefix=false;
+                    break;
+                }
+                else if(str[index]==current)
+                {
+
+                    continue;
+                }
+                else
+                { 
+                    isprefix=false;
+                    break;
+                }
+            }
+
+            if(isprefix)
+            {
+                index++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return strs[0].substr(0,index);
+        
+
+
+
+
+
+        
+    }
+};
