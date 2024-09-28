b3d4890afe0cd6c3dd92685de64ccd5badd93c4d
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-09-29 00:54:36 +0800

    Time: 186 ms (6.34%) | Memory: 78.1 MB (5.02%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        map<int,int> mp;
+        int count=0;
+
+        for(int &x:nums)
+        {
+            if(mp[k-x]>=1)
+            {
+                mp[k-x]--;
+                count++;
+            }
+            else
+            {
+                mp[x]++;
+            }
+        }
+        return count;
+        
+    }
+};
