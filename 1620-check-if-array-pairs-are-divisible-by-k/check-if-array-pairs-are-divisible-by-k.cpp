9a6a2dbe66f7b865b44220b47ab1ada74a590ccc
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-10-01 21:07:20 +0800

    Time: 179 ms (15.47%) | Memory: 74.7 MB (23.28%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -4,18 +4,15 @@ using namespace std;
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        int n=arr.size();
         map<int,int> mp;
 
-
-        for(int x:arr)
+        for(int &x:arr)
         {
             mp[(x % k + k) % k]++;
         }
 
-        for(pair<int,int> pr:mp)
+        for(pair<const int,int> pr:mp)
         {
-            cout<<pr.first<<" "<<pr.second<<endl;
             if(pr.first==0)
             {
                  if(pr.second%2!=0)
