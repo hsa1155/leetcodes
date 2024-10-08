68864b53dc8bee1655a33d99de1a47f041283774
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-10-08 21:53:21 +0800

    Time: 304 ms (94.95%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-select DISTINCT p.email as Email
-from Person as p
-join Person as d
-where p.id!=d.id and p.email=d.email
-
+select email as "Email"
+from Person
+group by email having count(email) > 1;
