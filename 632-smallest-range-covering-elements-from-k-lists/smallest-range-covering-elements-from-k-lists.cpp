3f4f4e8a0c264964078dba1d3af9e813f8e84d65
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-10-13 22:11:53 +0800

    Time: 39 ms (80.50%) | Memory: 22.5 MB (32.94%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -21,6 +21,7 @@ public:
             b1[i]=1;
         }
         sort(all.begin(),all.end());
+        //merge all number and its index and sort
         int n=all.size();
         int ansLength=INT_MAX;
         vector<int> ans(2);
@@ -28,16 +29,18 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            b2[all[i].second]=1;
+            b2[all[i].second]=1;//to  check if all index satisfied
             mp[all[i].second]++;
+            //for new element(value index pair) add it to window map to track
 
             while (mp[all[left].second]>1 )
             {
                 mp[all[left].second]--;
                 left++;
+                //while there is more than 1 element of [left]th array in current window,pop it
             }
 
-            if(b1==b2)
+            if(b1==b2)//fast check if all index satisfied
             {
                 if(all[i].first-all[left].first<ansLength)
                 {
