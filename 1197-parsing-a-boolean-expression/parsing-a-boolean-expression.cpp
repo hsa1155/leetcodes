b95a98a4368fe4ea81d20fe592555dac0b729994
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-10-20 18:09:21 +0800

    Time: 235 ms (5.21%) | Memory: 9.7 MB (64.32%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,112 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+
+        for(char ch:expression)
+        {
+            cout<<ch<<endl;
+            if(ch!=')')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                bool hastrue=false;
+                bool hasfalse=false;
+                while (!st.empty()&&(st.top()!='!'&&st.top()!='&'&&st.top()!='|'))
+                {
+                    if(st.top()=='t')
+                    {
+                        hastrue=true;
+                    }
+                    if(st.top()=='f')
+                    {
+                        hasfalse=true;
+                    }
+                    st.pop();
+                }
+
+                if(st.empty())
+                {
+                    cout<<"???"<<endl;
+                    continue;
+                }
+
+                if(st.top()=='!')
+                {
+                    if(hastrue&&hasfalse||(!hastrue&&!hasfalse))
+                    {
+                        cout<<"bugssssssss at !!!"<<endl;
+                        return false;
+                    }
+                    else if(hastrue)
+                    {
+                        st.pop();
+                        st.push('f');
+                    }
+                    else
+                    {
+                        st.pop();
+                        st.push('t');
+                    }
+                }
+                else if(st.top()=='&')
+                {
+                    if((!hastrue&&!hasfalse))
+                    {
+                        cout<<"bugssssssss at &&&"<<endl;
+                        return false;
+                    }
+                    else if(hasfalse)
+                    {
+                        st.pop();
+                        st.push('f');
+                    }
+                    else
+                    {
+                        st.pop();
+                        st.push('t');
+                    }
+
+                }
+                else if(st.top()=='|')
+                {
+                    if((!hastrue&&!hasfalse))
+                    {
+                        cout<<"bugssssssss at |||"<<endl;
+                        return false;
+                    }
+                    else if(hastrue)
+                    {
+                        st.pop();
+                        st.push('t');
+                    }
+                    else
+                    {
+                        st.pop();
+                        st.push('f');
+                    }
+
+                }
+                else
+                {
+                    cout<<"invalid!!!"<<endl;
+                    return false;
+                }
+                
+            }
+        }
+        if(st.top()=='t')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
