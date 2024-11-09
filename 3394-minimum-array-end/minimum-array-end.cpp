a08da9eee2f162c0707033a719cccc9a1d593f5c
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-11-09 21:42:01 +0800

    Time: 1753 ms (5.36%) | Memory: 8.5 MB (52.80%) - LeetSync

---
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long result = x;
+
+        // Step 1: Iterate n-1 times (since we already initialized result with
+        // x)
+        while (--n) {
+            // Step 2: Increment result and perform bitwise OR with x
+            result = (result + 1) | x;
+            //cout<<result<<endl;
+        }
+
+        return result;
+    }
+};
