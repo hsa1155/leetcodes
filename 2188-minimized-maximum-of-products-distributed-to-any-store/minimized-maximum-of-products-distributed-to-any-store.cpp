d3dc6b2758c13b801a461c29224d92262335fb5e
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-11-14 12:50:09 +0800

    Time: 1831 ms (5.24%) | Memory: 106.7 MB (13.71%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution
+{
+public:
+    int minimizedMaximum(int n, vector<int> &quantities)
+    {
+        int m=quantities.size();
+        // Define a custom comparator for the priority queue
+        // It sorts the pairs based on the ratio of their first to their second
+        // element
+        auto compareTypeStorePairs = [](pair<int, int> &a, pair<int, int> &b)
+        {
+            return (long long)a.first * b.second <
+                   (long long)a.second * b.first;
+        };
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(compareTypeStorePairs)> pq;
+
+        for(int x:quantities)
+        {
+            pq.push({x,1});
+        }
+
+        for(int i=0;i<n-m;i++)
+        {
+            pair<int,int> current=pq.top();
+            pq.pop();
+            current.second++;
+            pq.push(current);
+        }
+
+        return ceil((double)pq.top().first/(double)pq.top().second);
+    }
+};
+
+// 15 10 10 1 0 0 0
+// 8 10 10 1 7 0 0
+// 8 5 10 1 7 5 0
+// 8 5 5 1 7 5 5
+// 11 6 0
+// 6 6 5
+//// 8 5 5 1 7 5 5 0
+//// 5 5 5 1 5 5 5 5
