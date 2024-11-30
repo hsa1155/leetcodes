6abeb2871388193d3881fabad31cc7b315b2f9fe
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2024-11-30 16:48:28 +0800

    Time: 427 ms (22.17%) | Memory: 151 MB (7.49%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
@@ -3,14 +3,14 @@ class Solution:
         from collections import defaultdict, deque
 
         adjacencyMatrix = defaultdict(deque)
-        inDegree, outDegree = defaultdict(int), defaultdict(int)
+        Degree=defaultdict(int)
 
         # Build the adjacency list and track in-degrees and out-degrees
         for pair in pairs:
             start, end = pair
             adjacencyMatrix[start].append(end)
-            outDegree[start] += 1
-            inDegree[end] += 1
+            Degree[start]+=1
+            Degree[end]-=1
 
         result = []
 
@@ -20,17 +20,12 @@ class Solution:
                 visit(nextNode)
             result.append(node)
 
-        # Find the start node (outDegree == 1 + inDegree )
-        startNode = -1
-        for node in outDegree:
-            if outDegree[node] == inDegree[node] + 1:
+        # Find the start node
+        startNode = pairs[0][0]
+        for node in Degree:
+            if Degree[node]==1:
                 startNode = node
                 break
-
-        # If no such node exists, start from the first pair's first element
-        if startNode == -1:
-            startNode = pairs[0][0]
-
         # Start DFS traversal
         visit(startNode)
 
