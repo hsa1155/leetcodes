609f839c2360415c743b14701164bcf8ed516bcd
hsa1155 <63193260+hsa1155@users.noreply.github.com>
2025-01-07 15:18:57 +0800

    Time: 4 ms (28.54%) | Memory: 17.7 MB (28.23%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -3,13 +3,10 @@ from collections import *
 import math
 class Solution:
     def stringMatching(self, words: list[str]) -> list[str]:
-        ans=[]
-        for current in range(len(words)):
-            for other in range(len(words)):
-                if current==other:
-                    continue
-                if words[current] in words[other]:
-                    ans.append(words[current])
-                    break
+        # 將所有字串連接成一個大字串,用特殊字元分隔
+        text = ' '.join(words)
         
-        return ans
+        # 只要檢查一個字串在大字串中出現超過1次,就代表它是某個字串的子字串
+        # 但要排除它自己那一次
+        return [word for word in words 
+                if text.count(word) > 1]
